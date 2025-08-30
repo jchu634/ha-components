25f1cb8e94a094f03ce478330e42ce6c0892fea7
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-08-31 02:08:44 +1200

    Removed Unused files left over from template

--- api_debug/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from api-debug!")
-
-
-if __name__ == "__main__":
-    main()
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import * as React from "react";
-import { OpenInV0Button } from "@/components/open-in-v0-button";
-import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world";
 import { Camera } from "@/components/ha-ui/camera";
 
 export default function Home() {
@@ -19,18 +17,7 @@ export default function Home() {
                 <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
                     <div className="flex items-center justify-between">
                         <h2 className="text-sm text-muted-foreground sm:pl-3">
-                            A simple hello world component
-                        </h2>
-                        <OpenInV0Button name="hello-world" className="w-fit" />
-                    </div>
-                    <div className="flex items-center justify-center min-h-[400px] relative">
-                        <HelloWorld />
-                    </div>
-                </div>
-                <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-sm text-muted-foreground sm:pl-3">
-                            A Camera Feed Component
+                            A Simple Camera Component
                         </h2>
                         <OpenInV0Button name="hello-world" className="w-fit" />
                     </div>
