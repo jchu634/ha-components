933f25eac0b0badd08fe847cb3ae338ce6d0a8d7
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-09-02 22:14:58 +1200

    Rebased on a new Create Fumadocs foundation

--- source.config.ts
+++ source.config.ts
@@ -1,5 +1,23 @@
-import { defineDocs } from "fumadocs-mdx/config";
+import {
+  defineConfig,
+  defineDocs,
+  frontmatterSchema,
+  metaSchema,
+} from 'fumadocs-mdx/config';
 
+// You can customise Zod schemas for frontmatter and `meta.json` here
+// see https://fumadocs.dev/docs/mdx/collections#define-docs
 export const docs = defineDocs({
-    dir: "content/docs",
+  docs: {
+    schema: frontmatterSchema,
+  },
+  meta: {
+    schema: metaSchema,
+  },
+});
+
+export default defineConfig({
+  mdxOptions: {
+    // MDX options
+  },
 });
