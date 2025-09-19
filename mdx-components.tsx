4894a111fabd8fe7165260ef6af4c0e2436a1d48
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-09-20 04:33:54 +1200

    Added Fumadocs Tab and Accordion components to namespace

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,14 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
+import * as AccordionComponents from "fumadocs-ui/components/accordion";
+import type { MDXComponents } from "mdx/types";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-  return {
-    ...defaultMdxComponents,
-    ...components,
-  };
+    return {
+        ...defaultMdxComponents,
+        ...TabsComponents,
+        ...AccordionComponents,
+        ...components,
+    };
 }
