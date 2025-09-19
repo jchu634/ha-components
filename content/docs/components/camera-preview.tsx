05e3730e5bb7c9286c688ded0cd71ff0de1a10c9
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-09-20 04:35:27 +1200

    Added documentatioin for camera component

---
+++ content/docs/components/camera-preview.tsx
@@ -0,0 +1,30 @@
+"use client";
+import {
+    MediaController,
+    MediaControlBar,
+    MediaPlayButton,
+    MediaMuteButton,
+    MediaVolumeRange,
+    MediaFullscreenButton,
+} from "media-chrome/react";
+export default function CameraPreview() {
+    return (
+        <MediaController className="h-fit ">
+            <video
+                slot="media"
+                tabIndex={-1} // This is added for hydration as this is automatically injected
+                className="aspect-video"
+                src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            />
+            <MediaControlBar className="flex w-full justify-between px-4 bg-black">
+                <MediaPlayButton className="bg-black px-2 hover:bg-slate-800" />
+
+                <div className="text-white">
+                    <MediaMuteButton className="bg-black p-2 hover:bg-slate-800"></MediaMuteButton>
+                    <MediaVolumeRange className="bg-black px-2 hover:bg-slate-800"></MediaVolumeRange>
+                    <MediaFullscreenButton className="bg-black px-2 h-full hover:bg-slate-800"></MediaFullscreenButton>
+                </div>
+            </MediaControlBar>
+        </MediaController>
+    );
+}
