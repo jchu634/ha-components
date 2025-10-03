ff887f9c75eabb1babad46bb88e98415a7b037fc
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-10-03 20:01:46 +1300

    Made ApiLink button in docs slug

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,51 +1,58 @@
-import { source } from '@/lib/source';
-import {
-  DocsBody,
-  DocsDescription,
-  DocsPage,
-  DocsTitle,
-} from 'fumadocs-ui/page';
-import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import { createRelativeLink } from 'fumadocs-ui/mdx';
-import { getMDXComponents } from '@/mdx-components';
+import { source } from "@/lib/source";
+import { DocsBody, DocsDescription, DocsPage, DocsTitle } from "fumadocs-ui/page";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { createRelativeLink } from "fumadocs-ui/mdx";
+import { getMDXComponents } from "@/mdx-components";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
-export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export default async function Page(props: PageProps<"/docs/[[...slug]]">) {
+    const params = await props.params;
+    const page = source.getPage(params.slug);
+    if (!page) notFound();
 
-  const MDXContent = page.data.body;
+    const MDXContent = page.data.body;
 
-  return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription>{page.data.description}</DocsDescription>
-      <DocsBody>
-        <MDXContent
-          components={getMDXComponents({
-            // this allows you to link to other pages with relative file paths
-            a: createRelativeLink(source, page),
-          })}
-        />
-      </DocsBody>
-    </DocsPage>
-  );
+    return (
+        <DocsPage toc={page.data.toc} full={page.data.full}>
+            <div className="flex items-center space-x-4">
+                <DocsTitle>{page.data.title} </DocsTitle>
+                {page.data.apiLink && (
+                    <Button className="outline w-20" size="sm" variant="secondary" asChild>
+                        <Link href={page.data.apiLink} rel="noreferrer noopener" target="_blank">
+                            API
+                            <ExternalLink />
+                        </Link>
+                    </Button>
+                )}
+            </div>
+            <DocsDescription>{page.data.description}</DocsDescription>
+
+            <DocsBody>
+                <MDXContent
+                    components={getMDXComponents({
+                        // this allows you to link to other pages with relative file paths
+                        a: createRelativeLink(source, page),
+                    })}
+                />
+            </DocsBody>
+        </DocsPage>
+    );
 }
 
 export async function generateStaticParams() {
-  return source.generateParams();
+    return source.generateParams();
 }
 
-export async function generateMetadata(
-  props: PageProps<'/docs/[[...slug]]'>,
-): Promise<Metadata> {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
-  if (!page) notFound();
+export async function generateMetadata(props: PageProps<"/docs/[[...slug]]">): Promise<Metadata> {
+    const params = await props.params;
+    const page = source.getPage(params.slug);
+    if (!page) notFound();
 
-  return {
-    title: page.data.title,
-    description: page.data.description,
-  };
+    return {
+        title: page.data.title,
+        description: page.data.description,
+    };
 }
