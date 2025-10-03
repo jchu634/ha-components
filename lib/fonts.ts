856ad5fd448ab228e01be68fb3234c0c7ada2534
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-10-03 19:17:53 +1300

    Added fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Lexend_Deca, Funnel_Display } from "next/font/google";
+
+export const lexend = Lexend_Deca({
+    subsets: ["latin"],
+});
+export const funnel = Funnel_Display({
+    subsets: ["latin"],
+});
