2802fd4ca144d5977775cfef9e5c3779b11ba3ee
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-10-04 01:38:22 +1300

    Added placeholder entity-types file for build reasons

---
+++ types/entity-types.ts
@@ -0,0 +1,11 @@
+// AUTO-GENERATED FILE - DO NOT EDIT
+export type EntityId =
+    /**
+     * Placeholder
+     */
+    "placeholder.placeholder";
+
+export interface HAEntity {
+    entity_id: EntityId;
+    friendly_name?: string;
+}
