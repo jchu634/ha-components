ab89f27aec6a604edb2692352d9c63fb4ea87cfb
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-10-03 20:01:32 +1300

    Extenced fumadocs schema to add apiLink

--- source.config.ts
+++ source.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, defineDocs, frontmatterSchema, metaSchema } from "fumadocs-mdx/config";
+import { z } from "zod";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections#define-docs
 export const docs = defineDocs({
     docs: {
-        schema: frontmatterSchema,
+        schema: frontmatterSchema.extend({
+            apiLink: z.string().optional(),
+        }),
     },
     meta: {
         schema: metaSchema,
