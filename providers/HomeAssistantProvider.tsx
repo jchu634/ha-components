524859289505dfc81326d3c00081837e3b0982f0
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-12-24 01:10:21 +1300

    Annotated ReactNode with type for vite lint rules

--- providers/HomeAssistantProvider.tsx
+++ providers/HomeAssistantProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useEffect, useState } from "react";
+import { type ReactNode, useEffect, useState } from "react";
 import { haWebSocket } from "@/lib/haWebsocket";
 import { getAccessToken, login, exchangeCodeForToken, ENV } from "@/lib/haAuth";
 
